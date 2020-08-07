f05598c9f39f25640da592f7aff0a265add063ad
euniceyoh <euniceohsj@gmail.com>
2020-08-06 20:56:36 -0700

    Initial Commit

---
+++ HuffmanCoding/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HuffmanCoding
+//
+//  Created by Eunice Oh on 8/6/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
